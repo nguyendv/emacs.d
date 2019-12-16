37e539eb805a8d574a0bf329ef8de137497f78d7
Dave Nguyen <dv@dvnguyen.com>
2019-12-16 10:50:57 -0600

    org-mode: add SOMEDAY state for TODO items

--- init.el
+++ init.el
@@ -21,6 +21,10 @@
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-store-agenda)
 (global-set-key (kbd "C-c c") 'org-store-capture)
+;; TODO workflow states
+(setq org-todo-keywords
+  '((sequence "TODO" "SOMEDAY" "|" "DONE")))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
